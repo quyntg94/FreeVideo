7c64acf6097f8e2e159fa4c22036f534ee784c6d
quynth94 <giaquy.ptit@gmail.com>
2017-07-31 00:34:15 +0700

    no message

--- app.py
+++ app.py
@@ -6,17 +6,38 @@ import os
 
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-VIDEO_PATH = os.path.join(APP_ROOT, "video.json")
+COOK_PATH = os.path.join(APP_ROOT, "cook.json")
+MUSIC_PATH = os.path.join(APP_ROOT, "music.json")
+VLOG_PATH = os.path.join(APP_ROOT, "vlog.json")
+COMEDY_PATH = os.path.join(APP_ROOT, "comedy.json")
 
 app = Flask(__name__)
 api = Api(app)
 
-class VideoListRes(Resource):
+class CookListRes(Resource):
     def get(self):
-        with codecs.open(VIDEO_PATH, 'r', 'utf-8-sig') as data_file:
+        with codecs.open(COOK_PATH, 'r', 'utf-8-sig') as data_file:
             return json.load(data_file)
 
-api.add_resource(VideoListRes, "/api/video")
+class MusicListRes(Resource):
+    def get(self):
+        with codecs.open(MUSIC_PATH, 'r', 'utf-8-sig') as data_file:
+            return json.load(data_file)
+
+class VlogListRes(Resource):
+    def get(self):
+        with codecs.open(VLOG_PATH, 'r', 'utf-8-sig') as data_file:
+            return json.load(data_file)
+            
+class ComedyListRes(Resource):
+    def get(self):
+        with codecs.open(COMEDY_PATH, 'r', 'utf-8-sig') as data_file:
+            return json.load(data_file)
+
+api.add_resource(CookListRes, "/api/cook")
+api.add_resource(MusicListRes, "/api/music")
+api.add_resource(VlogListRes, "/api/vlog")
+api.add_resource(ComedyListRes, "/api/comedy")
 
 if __name__ == '__main__':
     app.run()
